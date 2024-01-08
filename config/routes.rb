20bc6da57967c52710e28997607f694ed9194839
Oshie <oshie0115@gmail.com>
2024-01-08 11:43:37 +0500

    Add user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  devise_for :users, path:'', path_names:{
-    sign_in: 'login',
-    sign_out:'logout',
-    registration:'signup'
-  },
-  controllers:{
-    sessions:'users/sessions',
-    registrations: 'users/registrations'
-  }
+  devise_for :users, path: '', path_names: {
+                                 sign_in: 'login',
+                                 sign_out: 'logout',
+                                 registration: 'signup'
+                               },
+                     controllers: {
+                       sessions: 'users/sessions',
+                       registrations: 'users/registrations'
+                     }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
